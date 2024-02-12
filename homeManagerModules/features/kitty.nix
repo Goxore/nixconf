4b48ae017e686ad53124c587979ec2e9b3ad51bc
Yurii <ggooxxoorree@gmail.com>
2024-02-12 15:56:46 +0100

    kitty scrollback

--- homeManagerModules/features/kitty.nix
+++ homeManagerModules/features/kitty.nix
@@ -7,12 +7,12 @@
     enable = true;
     font.name = "JetBrainsMono Nerd Font Mono";
     font.size = 15;
-    keybindings = {
-      "kitty_mod+h" = "neighboring_window left";
-      "kitty_mod+l" = "neighboring_window right";
-      "kitty_mod+j" = "neighboring_window down";
-      "kitty_mod+k" = "neighboring_window up";
-    };
+    # keybindings = {
+    #   "kitty_mod+h" = "neighboring_window left";
+    #   "kitty_mod+l" = "neighboring_window right";
+    #   "kitty_mod+j" = "neighboring_window down";
+    #   "kitty_mod+k" = "neighboring_window up";
+    # };
     shellIntegration.enableZshIntegration = true;
     settings = with config.colorScheme.colors; {
       enable_audio_bell = "no";
@@ -69,6 +69,19 @@
       # = "white (fg4/fg3)";
       color7 = "#${base05}";
       color15 = "#${base06}";
+
+      # scrollback_pager = ''nvim -c "set signcolumn=no showtabline=0" -c "silent write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - "'';
+
+      allow_remote_control = "yes";
+      listen_on = "unix:/tmp/kitty";
+      shell_integration = "enabled";
     };
+    extraConfig = ''
+      # GENERATED
+      action_alias kitty_scrollback_nvim kitten /home/${config.home.username}/.local/share/nvim/lazy/kitty-scrollback.nvim/python/kitty_scrollback_nvim.py
+      map kitty_mod+h kitty_scrollback_nvim
+      map kitty_mod+g kitty_scrollback_nvim --config ksb_builtin_last_cmd_output
+      mouse_map ctrl+shift+right press ungrabbed combine : mouse_select_command_output : kitty_scrollback_nvim --config ksb_builtin_last_visited_cmd_output
+    '';
   };
 }
