2e3fd5328529dc4815461e9ee4f82d78dfe98937
yurii <yurii@goxore.com>
2025-02-16 21:46:04 +0100

    gruvbox plus

--- homeManagerModules/features/gtk/default.nix
+++ homeManagerModules/features/gtk/default.nix
@@ -1,15 +1,15 @@
 {
-  pkgs,
   config,
+  inputs,
   ...
 }: let
-  gruvboxPlus = import ./gruvbox-plus.nix {inherit pkgs;};
+  pkgs = inputs.nixpkgsold.legacyPackages."x86_64-linux";
 in {
   home.file = {
-    ".local/share/icons/GruvboxPlus".source = "${gruvboxPlus}";
+    ".local/share/icons/GruvboxPlus".source = "${pkgs.gruvbox-plus-icons}/share/icons/Gruvbox-Plus-Dark";
   };
 
   gtk.enable = true;
-  gtk.iconTheme.package = gruvboxPlus;
+  gtk.iconTheme.package = pkgs.gruvbox-plus-icons;
   gtk.iconTheme.name = "GruvboxPlus";
 }
