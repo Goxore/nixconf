e5d0b3de273093557d3969be72487a3a6b1b96d1
Yurii <yurii@goxore.com>
2025-10-11 19:12:57 +0200

    git name&mail

--- modules/wrappedPrograms/git.nix
+++ modules/wrappedPrograms/git.nix
@@ -4,9 +4,11 @@
     packages.git = inputs.wrappers.lib.makeWrapper {
       inherit pkgs;
       package = pkgs.git;
-      env = {
+      env = rec {
         GIT_AUTHOR_NAME = "Yurii";
         GIT_AUTHOR_EMAIL = "yurii@goxore.com";
+        GIT_COMMITTER_NAME = GIT_AUTHOR_NAME;
+        GIT_COMMITTER_EMAIL = GIT_AUTHOR_EMAIL;
       };
     };
   };
