04bb601cac5e63462a9e7d246c3a76e6df8f6538
Yurii <yurii@goxore.com>
2025-12-31 18:42:02 +0200

    more snippets

--- modules/wrappedPrograms/neovim/after/ftplugin/vjxl.lua
+++ modules/wrappedPrograms/neovim/after/ftplugin/vjxl.lua
@@ -67,6 +67,72 @@ local function next_unused_from_buffer()
     return pick_next_unused(used)
 end
 
+--- @return table<number, boolean>
+local function collect_used_p_numbers()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+
+    local used = {}
+
+    vim.iter(lines)
+        :map(function(line)
+            return vim.iter(line:gmatch("@p(%d+)"))
+                :map(tonumber)
+                :totable()
+        end)
+        :flatten()
+        :each(function(n)
+            used[n] = true
+        end)
+
+    return used
+end
+
+--- @return number
+local function next_p_number_from_buffer()
+    local used = collect_used_p_numbers()
+
+    return vim.iter(vim.tbl_keys(used))
+        :map(tonumber)
+        :fold(0, math.max) + 1
+end
+
+--- @param nav_method string
+local function nav_snippet(nav_method)
+    return S.fmt(
+        [[
+await @nav.{}(@p{} Rect {{ size: [1920,1080]
+  {}
+}})
+        ]],
+        {
+            S.t(nav_method),
+            S.f(function() return tostring(next_p_number_from_buffer()) end),
+            S.i(1)
+        }
+    )
+end
+
+
+--- @return number|nil
+local function latest_p_before_cursor()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, cursor_line - 1, false)
+
+    local nums = vim.iter(lines)
+        :map(function(line)
+            return vim.iter(line:gmatch("@p(%d+)"))
+                :map(tonumber)
+                :totable()
+        end)
+        :flatten()
+        :totable()
+
+    if #nums == 0 then return nil end
+    return nums[#nums]
+end
+
 --- @type LuaSnip.Opts.AddSnippets
 local opts = {
     key = "vjxlmain"
@@ -113,5 +179,41 @@ ls.add_snippets("vjxl", {
         {
             S.i(1)
         })),
+
+    S.s("right", nav_snippet("right")),
+
+    S.s("push", nav_snippet("push")),
+
+    S.s("size1", { S.t("size: [1920,1080]") }),
+    S.s("size2", { S.t("size: [1800,960]") }),
+    S.s("size3", { S.t("size: [1720,880]") }),
+
+    S.s("jump", S.fmt([[
+    await @vj.jump("{}")
+    ]], { S.i(1, "idle") })),
+
+    S.s("all", S.fmt([[
+    await allPrime([
+      {},
+    ])
+    ]], { S.i(1, "idle") })),
+
+    S.s("add",
+        S.fmt(
+            [[
+await addReveal(@p{}, Rect {{
+  Opacity {{
+      {}
+  }}
+}})
+        ]],
+            {
+                S.f(function()
+                    local n = latest_p_before_cursor()
+                    return tostring(n or 1)
+                end),
+                S.i(1)
+            }
+        ))
 
 }, opts)
