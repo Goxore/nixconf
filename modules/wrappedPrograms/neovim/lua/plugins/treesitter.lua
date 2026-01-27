f7bc4795b205fd40ae039995f1a8e53fff85061b
Yurii <yurii@goxore.com>
2026-01-27 16:01:42 +0200

    highlighting fix

--- modules/wrappedPrograms/neovim/lua/plugins/treesitter.lua
+++ modules/wrappedPrograms/neovim/lua/plugins/treesitter.lua
@@ -1,5 +1,11 @@
 return {
     "nvim-treesitter",
     after = function()
+        -- fixes vjxl highlighting
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
     end
 }
