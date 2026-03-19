2ee71032c0d5554c208318db76cdc41cbf4dfb7c
Yurii <yurii@goxore.com>
2026-03-19 21:46:26 +0200

    wow

--- modules/nixos/features/gaming.nix
+++ modules/nixos/features/gaming.nix
@@ -1,4 +1,4 @@
-{
+{self, ...}: {
   flake.nixosModules.gaming = {
     pkgs,
     lib,
@@ -10,6 +10,13 @@
       gamemode.enable = true;
       gamescope.enable = true;
       steam = {
+        package = pkgs.steam.override {
+          extraProfile = ''
+            unset TZ
+            # Allows Monado/WiVRn to be used
+            export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
+          '';
+        };
         enable = true;
         extraCompatPackages = with pkgs; [
           proton-ge-bin
@@ -46,6 +53,7 @@
 
       lsfg-vk
       lsfg-vk-ui
+      self.packages.${pkgs.system}.wow-launcher
     ];
 
     services.zerotierone.enable = true;
@@ -65,5 +73,58 @@
 
       ".config/heroic"
     ];
+  };
+
+  perSystem = {pkgs, ...}: {
+    packages.wow-launcher = pkgs.writeShellApplication {
+      name = "wow-launcher";
+
+      runtimeInputs = with pkgs; [
+        (wineWow64Packages.full.override {
+          wineRelease = "staging";
+          mingwSupport = true;
+        })
+        winetricks
+        vulkan-loader
+        dxvk
+      ];
+
+      # todo: fix
+      text = ''
+        export WINEPREFIX="$HOME/Games/Wow"
+        export WINEARCH=win64
+        export WINEDEBUG="-all"
+        export DRI_PRIME=1
+        export DXVK_HUD=1
+        export DXVK_DEVICE_SELECT=1
+
+        BNET_EXE="$WINEPREFIX/drive_c/Program Files (x86)/Battle.net/Battle.net.exe"
+        WOW_EXE="$WINEPREFIX/drive_c/Program Files (x86)/World of Warcraft/_retail_/Wow.exe"
+        INSTALLER="Battle.net-Setup.exe"
+
+        if [ ! -d "$WINEPREFIX" ]; then
+          echo "Initializing new Wine prefix..."
+          mkdir -p "$WINEPREFIX"
+          wineboot -u
+        fi
+
+        if [ -f "$WOW_EXE" ]; then
+          echo "Launching WoW via DXVK..."
+          wine "$WOW_EXE"
+          exit 0
+        fi
+
+        if [ ! -f "$BNET_EXE" ]; then
+          if [ -f "$INSTALLER" ]; then
+            wine "$INSTALLER"
+          else
+            echo "Installer not found. Please download Battle.net-Setup.exe"
+            exit 1
+          fi
+        else
+          wine "$BNET_EXE"
+        fi
+      '';
+    };
   };
 }
