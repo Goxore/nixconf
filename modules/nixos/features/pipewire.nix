73b08068c6519322aca1878668f14848cce7fd23
Yurii <yurii@goxore.com>
2026-03-19 21:46:26 +0200

    test new noise removal

--- modules/nixos/features/pipewire.nix
+++ modules/nixos/features/pipewire.nix
@@ -5,7 +5,6 @@
       "SUPER + d"."s".package = pkgs.pwvucontrol;
     };
 
-
     persistance.cache.directories = [
       ".local/state/wireplumber"
     ];
@@ -19,43 +18,78 @@
       jack.enable = true;
 
       extraConfig = {
-        # https://discourse.nixos.org/t/pipewire-rnnoise-module-wont-work/58975/12
+        # cooler denoising
         pipewire."99-input-denoising" = {
           "context.modules" = [
             {
-              name = "libpipewire-module-filter-chain";
-              args = {
-                "node.description" = "Noise Canceling source";
-                "media.name" = "Noise Canceling source";
+              "name" = "libpipewire-module-filter-chain";
+              "args" = {
+                "node.description" = "DeepFilter Noise Cancelling Source";
+                "media.name" = "DeepFilter Noise Cancelling Source";
                 "filter.graph" = {
-                  nodes = [
+                  "nodes" = [
                     {
-                      type = "ladspa";
-                      name = "rnnoise";
-                      plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-                      label = "noise_suppressor_mono";
-                      control = {
-                        "VAD Threshold (%)" = 50.0;
-                        "VAD Grace Period (ms)" = 200;
-                        "Retroactive VAD Grace (ms)" = 0;
-                      };
+                      "type" = "ladspa";
+                      "name" = "DeepFilter Mono";
+                      "plugin" = "${pkgs.deepfilternet}/lib/ladspa/libdeep_filter_ladspa.so";
+                      "label" = "deep_filter_mono";
+                      # "control" = {
+                      #   "Attenuation Limit (dB)" = cfg.source.attenuation;
+                      # };
                     }
                   ];
                 };
+                "audio.rate" = 48000;
                 "capture.props" = {
-                  "node.name" = "capture.rnnoise_source";
+                  "node.name" = "deep_filter_mono_input";
                   "node.passive" = true;
-                  "audio.rate" = 48000;
                 };
                 "playback.props" = {
-                  "node.name" = "rnnoise_source";
+                  "node.name" = "deep_filter_mono_output";
                   "media.class" = "Audio/Source";
-                  "audio.rate" = 48000;
                 };
               };
             }
           ];
         };
+
+        # https://discourse.nixos.org/t/pipewire-rnnoise-module-wont-work/58975/12
+        # pipewire."99-input-denoising" = {
+        #   "context.modules" = [
+        #     {
+        #       name = "libpipewire-module-filter-chain";
+        #       args = {
+        #         "node.description" = "Noise Canceling source";
+        #         "media.name" = "Noise Canceling source";
+        #         "filter.graph" = {
+        #           nodes = [
+        #             {
+        #               type = "ladspa";
+        #               name = "rnnoise";
+        #               plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+        #               label = "noise_suppressor_mono";
+        #               control = {
+        #                 "VAD Threshold (%)" = 50.0;
+        #                 "VAD Grace Period (ms)" = 200;
+        #                 "Retroactive VAD Grace (ms)" = 0;
+        #               };
+        #             }
+        #           ];
+        #         };
+        #         "capture.props" = {
+        #           "node.name" = "capture.rnnoise_source";
+        #           "node.passive" = true;
+        #           "audio.rate" = 48000;
+        #         };
+        #         "playback.props" = {
+        #           "node.name" = "rnnoise_source";
+        #           "media.class" = "Audio/Source";
+        #           "audio.rate" = 48000;
+        #         };
+        #       };
+        #     }
+        #   ];
+        # };
       };
     };
   };
