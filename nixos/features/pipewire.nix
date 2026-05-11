78e82d766543c8e4286b8aef6dd70411c2934a20
Yurii <yurii@goxore.com>
2026-05-11 12:43:56 +0300

    proper audio

--- nixos/features/pipewire.nix
+++ nixos/features/pipewire.nix
@@ -17,42 +17,87 @@
       pulse.enable = true;
       jack.enable = true;
 
+      configPackages = [pkgs.deepfilternet];
       extraLadspaPackages = [pkgs.deepfilternet];
       extraConfig = {
+        pipewire = {
+          "90-defaults" = {
+            "context.properties" = {
+              "clock.power-of-two-quantum" = true;
+              "core.daemon" = true;
+              "core.name" = "pipewire-0";
+              "link.max-buffers" = 16;
+              "settings.check-quantum" = true;
+
+              "default.clock.rate" = 96000;
+              "default.clock.allowed-rates" = [44100 48000 88200 96000 192000 352800 384000];
+              "default.clock.quantum" = 256;
+              "default.clock.min-quantum" = 32;
+              "default.clock.max-quantum" = 4096;
+            };
+            "stream.properties" = {
+              "resample.quality" = 10;
+            };
+          };
+
+          "90-disable-bell" = {
+            "context.properties" = {
+              "module.x11.bell" = false;
+            };
+          };
+        };
+
+        pipewire-pulse = {
+          "90-defaults" = {
+            "context.spa-libs" = {
+              "audio.convert.*" = "audioconvert/libspa-audioconvert";
+              "support.*" = "support/libspa-support";
+            };
+
+            "stream.properties" = {
+              "resample.quality" = 10;
+            };
+
+            "pulse.properties" = {
+              "server.address" = ["unix:native"];
+            };
+          };
+        };
+
         # cooler denoising
-        # pipewire."99-input-denoising" = {
-        #   "context.modules" = [
-        #     {
-        #       "name" = "libpipewire-module-filter-chain";
-        #       "args" = {
-        #         "node.description" = "DeepFilter Noise Cancelling Source";
-        #         "media.name" = "DeepFilter Noise Cancelling Source";
-        #         "filter.graph" = {
-        #           "nodes" = [
-        #             {
-        #               "type" = "ladspa";
-        #               "name" = "DeepFilter Mono";
-        #               "plugin" = "${pkgs.deepfilternet}/lib/ladspa/libdeep_filter_ladspa.so";
-        #               "label" = "deep_filter_mono";
-        #               "control" = {
-        #                 "Attenuation Limit (dB)" = 100;
-        #               };
-        #             }
-        #           ];
-        #         };
-        #         "audio.rate" = 48000;
-        #         "capture.props" = {
-        #           "node.name" = "deep_filter_mono_input";
-        #           "node.passive" = true;
-        #         };
-        #         "playback.props" = {
-        #           "node.name" = "deep_filter_mono_output";
-        #           "media.class" = "Audio/Source";
-        #         };
-        #       };
-        #     }
-        #   ];
-        # };
+        pipewire."99-input-denoising" = {
+          "context.modules" = [
+            {
+              "name" = "libpipewire-module-filter-chain";
+              "args" = {
+                "node.description" = "DeepFilter Noise Cancelling Source";
+                "media.name" = "DeepFilter Noise Cancelling Source";
+                "filter.graph" = {
+                  "nodes" = [
+                    {
+                      "type" = "ladspa";
+                      "name" = "DeepFilter Mono";
+                      "plugin" = "libdeep_filter_ladspa";
+                      "label" = "deep_filter_mono";
+                      "control" = {
+                        "Attenuation Limit (dB)" = 100;
+                      };
+                    }
+                  ];
+                };
+                "audio.rate" = 48000;
+                "capture.props" = {
+                  "node.name" = "deep_filter_mono_input";
+                  "node.passive" = true;
+                };
+                "playback.props" = {
+                  "node.name" = "deep_filter_mono_output";
+                  "media.class" = "Audio/Source";
+                };
+              };
+            }
+          ];
+        };
 
         # https://discourse.nixos.org/t/pipewire-rnnoise-module-wont-work/58975/12
         # pipewire."99-input-denoising" = {
