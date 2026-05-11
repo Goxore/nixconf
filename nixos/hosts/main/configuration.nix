8eafd04a6a541fdc0113cd6375b374e915bbf519
Yurii <yurii@goxore.com>
2026-05-11 12:43:56 +0300

    repeater

--- nixos/hosts/main/configuration.nix
+++ nixos/hosts/main/configuration.nix
@@ -83,6 +83,21 @@
       zerotierone
 
       android-tools
+      self.packages."${pkgs.stdenv.hostPlatform.system}".ask
+      (rustPlatform.buildRustPackage
+        (finalAttrs: {
+          pname = "repeater";
+          version = "v0.1.10";
+
+          src = fetchFromGitHub {
+            owner = "shaankhosla";
+            repo = "repeater";
+            tag = finalAttrs.version;
+            hash = "sha256-8TWqY78w5cOirLnrBONiratAZQSQrYpp++5dXRlFlNo=";
+          };
+
+          cargoHash = "sha256-hngZ55o1YsnstBGjp8++9SsxwfUyu+X4YwZuzMupFTE=";
+        }))
     ];
 
     xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
