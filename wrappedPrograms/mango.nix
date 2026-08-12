604a4de714fbd1ab44e4188268fe6321ecee23b2
Yurii <yurii@goxore.com>
2026-08-12 18:44:48 +0300

    smart border

--- wrappedPrograms/mango.nix
+++ wrappedPrograms/mango.nix
@@ -98,7 +98,7 @@
         overviewgappi = 5;
         overviewgappo = 30;
 
-        no_border_when_single = 0;
+        no_border_when_single = 1;
         axis_bind_apply_timeout = 100;
         focus_on_activate = 0;
         idleinhibit_ignore_visible = 0;
